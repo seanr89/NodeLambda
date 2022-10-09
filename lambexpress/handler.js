34a348f2b61bb730ae03d997e3f56721f64d8366
Sean Rafferty <seanr@randox.com>
2022-10-09 17:24:11 +0100

    example notes

---
+++ lambexpress/handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+  //https://www.serverless.com/blog/serverless-express-rest-api/
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
